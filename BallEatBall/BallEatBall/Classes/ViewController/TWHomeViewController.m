f8cba4eda935f97c36b6b0c783dc2ad63beecd19
tianwei <tianwei223344@163.com>
2017-08-28 09:43:50 +0800

    初始化项目

---
+++ BallEatBall/BallEatBall/Classes/ViewController/TWHomeViewController.m
@@ -0,0 +1,23 @@
+//
+//  TWHomeViewController.m
+//  BallEatBall
+//
+//  Created by 田伟 on 2017/8/28.
+//  Copyright © 2017年 田伟. All rights reserved.
+//
+
+#import "TWHomeViewController.h"
+
+@interface TWHomeViewController ()
+
+@end
+
+@implementation TWHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = TWRandomColor;
+    
+}
+
+@end
